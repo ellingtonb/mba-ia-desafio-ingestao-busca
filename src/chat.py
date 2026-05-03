be9fe8b10baa7599f2578430ec40b1dcfc1e0a5e
Ellington Brambila <contato@ellingtonb.com>
2026-05-03 00:38:44 -0300

    add chat code

--- src/chat.py
+++ src/chat.py
@@ -1,12 +1,22 @@
+from commons import select_provider
 from search import search_prompt
 
+
 def main():
+    select_provider()
+
+    question = input("Digite sua pergunta: ")
+
     chain = search_prompt()
 
     if not chain:
         print("Não foi possível iniciar o chat. Verifique os erros de inicialização.")
         return
-    
+
+    result = chain.invoke({"pergunta": question})
+
+    print(f"\nRESPOSTA: {result.content}\n")
+
     pass
 
 if __name__ == "__main__":
